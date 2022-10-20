5aa55150b3752d125b7de305622199a7f6c64ab6
benitorubano1992 <rubanobenito@gmail.com>
2022-10-20 13:57:10 +0200

    complete milestone1,milestone2

---
+++ js/script.js
@@ -0,0 +1,68 @@
+console.log("pagina collegata");
+const teamMember=[
+    {
+        name:"Wayne Barnett",
+        role:"Founder & CEO	",
+        img:" wayne-barnett-founder-ceo.jpg"
+
+    },
+    {
+        name:"Angela Caroll",
+        role:"Chief Editor",
+        img:"angela-caroll-chief-editor.jpg "
+
+    },
+    {
+        name:"Walter Gordon",
+        role:"Office Manager",
+        img:"walter-gordon-office-manager.jpg"
+
+    },
+    {
+        name:"Angela Lopez",
+        role:"Social Media Manager",
+        img:"angela-lopez-social-media-manager.jpg"
+
+    },
+    {
+        name:"Scott Estrada",
+        role:"Developer",
+        img:"scott-estrada-developer.jpg"
+
+    },
+    {
+        name:"Barbara Ramos",
+        role:"Graphic Designer",
+        img:"barbara-ramos-graphic-designer.jpg"
+
+    },
+
+
+]
+	
+console.log(teamMember);
+//Milestone 1 Stampare su console le info per ogni oggetto dell'array team Member
+for(let member of teamMember){//scorro l'array di opggetti
+    
+    for(let key in member){//per ciascun oggetto alla volta scorro le sue chiavi e stampo il valore
+        console.log(member[key]);
+    }
+}
+//milestone 2 stampare le stesse inforamzioni sottoforma di stringhe sul dom
+
+const ulTeam=document.getElementById("list-member");
+
+for(let member of teamMember){
+const liMember=document.createElement("li");
+liMember.textContent=member.name;
+const ulIntern=document.createElement("ul");
+let result=""
+for(let key in member){
+    if(key !== "name"){
+        result+=`<li>${member[key]}</li>`
+    }
+}       
+ulIntern.innerHTML=result;
+liMember.append(ulIntern);
+ulTeam.append(liMember);
+}
